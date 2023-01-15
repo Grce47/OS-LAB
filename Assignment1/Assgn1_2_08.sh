1c9e0c5fc0445b2449850d17484353c5cad7aef1
umang-singla <umangsingla617@gmail.com>
2023-01-15 23:52:56 +0530

    corrected bug

--- Assignment1/Assgn1_2_08.sh
+++ Assignment1/Assgn1_2_08.sh
@@ -1,7 +1,7 @@
 isInvalidword()(while read p;do
-        [[ $1 =~ $p ]]&&{ echo 1;exit 1;};done<fruits.txt;echo 0;
+        [[ ${1,,} =~ ${p,,} ]]&&{ echo 1;exit 1;};done<fruits.txt;echo 0;
 )
 validate()(
-    (((${#1}<5||${#1}>20))||[[ ${1:0:1} != [a-z]&&${1:0:1} != [A-Z] ]]||! [[ "$1"=~^[0-9a-zA-Z]+$ ]]||! [[ "$1"=~[0-9] ]]||(($(isInvalidword $1)==1)))&&echo "NO"||echo "YES";
+    (((${#1}<5||${#1}>20))||[[ ${1:0:1} != [a-zA-Z] ]]||! [[ "$1" =~ ^[0-9a-zA-Z]+$ ]]||! [[ "$1" =~ [0-9] ]]||(($(isInvalidword $1)==1)))&&echo "NO"||echo "YES";
 )
 while read p;do validate $p;done<$1
