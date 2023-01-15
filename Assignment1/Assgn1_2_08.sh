6b268e977a2e04a93a52b82562264f647ae77f11
umang-singla <umangsingla617@gmail.com>
2023-01-15 18:01:28 +0530

    reduced size

--- Assignment1/Assgn1_2_08.sh
+++ Assignment1/Assgn1_2_08.sh
@@ -1,37 +1,7 @@
-isInvalidword()(
-    while read p
-    do
-        if [[ $1 =~ $p ]]
-        then
-            echo $p
-            echo 1
-            exit 1
-        fi  
-    done < fruits.txt
-    echo 0
+isInvalidword()(while read p;do
+        [[ $1 =~ $p ]]&&{ echo 1;exit 1;};done<fruits.txt;echo 0;
 )
 validate()(
-    tmp=$(isInvalidword $1)
-    if (( ${#1}<5||${#1}>20 ))
-    then
-        echo "NO"
-    elif [[ ${1:0:1} != [a-z]&&${1:0:1} != [A-Z] ]]
-    then
-        echo "NO"
-    elif ! [[ "$1"=~^[0-9a-zA-Z]+$ ]]
-    then
-        echo "NO"
-    elif ! [[ "$1"=~[0-9] ]]
-    then 
-        echo "NO"
-    elif (($tmp==1))
-    then
-        echo "NO"
-    else
-        echo "YES"
-    fi 
+    ((${#1}<5||${#1}>20))&&{ echo "NO";exit 1;};[[ ${1:0:1}!=[a-z]&&${1:0:1}!=[A-Z] ]]&&{ echo "NO";exit 1;};! [[ "$1"=~^[0-9a-zA-Z]+$ ]]&&{ echo "NO";exit 1;};! [[ "$1"=~[0-9] ]]&&{ echo "NO";exit 1;};(($(isInvalidword $1)==1))&&{ echo "NO";exit 1;};echo "YES"
 )
-while read p
-do
-    validate $p
-done < $1
+while read p;do validate $p;done<$1
