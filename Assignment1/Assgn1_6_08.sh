d6c88620590c329e9a16aa61f31ed6e3bc8df28d
grce47 <gracesharma47@gmail.com>
2023-01-15 15:10:13 +0530

    grace terminal

--- Assignment1/Assgn1_6_08.sh
+++ Assignment1/Assgn1_6_08.sh
@@ -1,28 +1,6 @@
-prime=(0 0)
-for i in {2..1000001}
-do 
-    prime[i]=1; 
-done
-for i in {2..1000001}
-do
-    if [ ${prime[i]} -eq 0 ]
-    then 
-        continue
-    fi
-    for (( j=i*i; j<1000001 ; j+=i ))
-    do
-        prime[j]=0;
-    done
-done
-echo -n "" > output.txt
-while read p
-do
-    for (( i=2;i<=$p;i++ ))
-    do 
-        if [ ${prime[i]} -eq 1 ]
-        then
-            echo -n $i" " 
-        fi    
-    done
-    echo
-done < input.txt >> output.txt
+prime=(0,0);for((i=2;i<1000001;i++));do
+prime[i]=1;done;for((i=2;i<1000001;i++));do
+((${prime[i]}==0))&&continue;for((j=i*i;j<1000001;j+=i));do
+prime[j]=0;done;done;echo -n"">output.txt;while read p;do
+for((i=2;i<=$p;i++));do 
+((${prime[i]}==1))&&echo -n $i" ";done;echo;done<input.txt>>output.txt
