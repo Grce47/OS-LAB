b209edf59728b0c811766c50d00b943478059411
umang-singla <umangsingla617@gmail.com>
2023-01-15 18:13:42 +0530

    reduced size

--- Assignment1/Assgn1_2_08.sh
+++ Assignment1/Assgn1_2_08.sh
@@ -2,6 +2,6 @@ isInvalidword()(while read p;do
         [[ $1 =~ $p ]]&&{ echo 1;exit 1;};done<fruits.txt;echo 0;
 )
 validate()(
-    ((${#1}<5||${#1}>20))&&{ echo "NO";exit 1;};[[ ${1:0:1}!=[a-z]&&${1:0:1}!=[A-Z] ]]&&{ echo "NO";exit 1;};! [[ "$1"=~^[0-9a-zA-Z]+$ ]]&&{ echo "NO";exit 1;};! [[ "$1"=~[0-9] ]]&&{ echo "NO";exit 1;};(($(isInvalidword $1)==1))&&{ echo "NO";exit 1;};echo "YES"
+    ((${#1}<5||${#1}>20))&&{ echo "NO";exit 1;};[[ ${1:0:1} != [a-z]&&${1:0:1} != [A-Z] ]]&&{ echo "NO";exit 1;};! [[ "$1"=~^[0-9a-zA-Z]+$ ]]&&{ echo "NO";exit 1;};! [[ "$1"=~[0-9] ]]&&{ echo "NO";exit 1;};(($(isInvalidword $1)==1))&&{ echo "NO";exit 1;};echo "YES"
 )
 while read p;do validate $p;done<$1
