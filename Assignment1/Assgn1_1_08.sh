9a24e65ef6c598b65ad1366a4eb4f590979797ff
umang-singla <umangsingla617@gmail.com>
2023-01-17 17:53:30 +0530

    resolved buf in q1

--- Assignment1/Assgn1_1_08.sh
+++ Assignment1/Assgn1_1_08.sh
@@ -1,12 +1,2 @@
-gcd() (
-    ! (( $1 % $2 )) && echo $2 || gcd $2 $(( $1 % $2 ))
-)
-ans=1
-while read p
-do
-    tmp=$( echo $p | rev )
-    mult=$(bc<<<"$ans*$tmp")
-    gc=$(bc<<<"$( gcd $ans $tmp )")  
-    ans=$(bc<<<"$mult/$gc")
-done < $1
-echo $ans
+gcd()(!(($1%$2))&&echo $2||gcd $2 $(($1%$2)));ans=1;while read p;do  
+tmp=$(echo $p|rev);mult=$(bc<<<"$ans*$tmp");gc=$(bc<<<"$(gcd $ans $tmp)");ans=$(bc<<<"$mult/$gc");done<$1;echo $ans
