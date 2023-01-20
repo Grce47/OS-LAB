398b65737b3bbb98fb0545bc3d2ff071d14a26ab
Sourabh Soumyakanta Das <96536342+Sourabh-S-Das@users.noreply.github.com>
2023-01-20 21:37:57 +0530

    Check plz

--- Assignment1/Assgn1_9_08.sh
+++ Assignment1/Assgn1_9_08.sh
@@ -1,4 +1,4 @@
 declare -A dept_dict name_dict;while read -a p;do
 dept_dict[${p[1]}]=$((dept_dict[${p[1]}]+1));name_dict[${p[0]}]=$((name_dict[${p[0]}]+1));done<$1;for k in ${!dept_dict[@]};do
-echo "$k ${dept_dict[$k]}";done |sort -k2rn,2;echo;cnt=0;for k in ${!name_dict[@]};do
+echo "$k ${dept_dict[$k]}";done |sort -k2rn,2;echo;for k in ${!name_dict[@]};do
 ((${name_dict[$k]}==1))&&cnt=$((cnt+1))||echo $k;done;echo $cnt;
