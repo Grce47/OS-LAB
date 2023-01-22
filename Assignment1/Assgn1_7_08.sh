371ad88c198fbb414c62a2d8071a6a34cc89fec8
Mradul Agrawal <mradul@kgpian.iitkgp.ac.in>
2023-01-22 11:18:33 +0530

    Update Assgn1_7_08.sh
    Fixed Bug for already existing files

--- Assignment1/Assgn1_7_08.sh
+++ Assignment1/Assgn1_7_08.sh
@@ -1,1 +1,1 @@
-mkdir -p $2;touch $2/{a..z}.txt;for line in $(cat $1/*.txt|sort);do echo "$line">>"$2/${line:0:1}.txt";done
+mkdir -p $2;rm $2/{a..z}.txt||true;touch $2/{a..z}.txt;for line in $(cat $1/*.txt|sort);do echo "$line">>"$2/${line:0:1}.txt";done
