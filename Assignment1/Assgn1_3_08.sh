e4e4f5532c49cde902c91ad5287efff1bf12f8d0
Umang Singla <73515380+umang-singla@users.noreply.github.com>
2023-02-03 17:50:25 +0530

    Add files via upload

---
+++ Assignment1/Assgn1_3_08.sh
@@ -0,0 +1,56 @@
+i=0
+while read line
+do
+    array[ $i ]="$line"        
+    (( i++ ))
+done < <(ls $1)
+
+dest=$2
+source=$1
+shift
+shift
+
+RO=1
+COL=0
+
+for i in "${array[@]}"
+do
+    sample=$(jq --slurp . < $source/$i)
+    file=$(echo $i | sed "s/jsonl/csv/")
+    rm -f "$dest/$file"
+    t=0
+    flag=$((${#@}-1))
+    string="\""
+    for arg in ${@}; do
+        if (( $t != $flag )) 
+        then
+            string=$string"$arg\", \"" 
+        else
+            string=$string"$arg\""
+        fi
+        (( t++ ))
+    done
+    echo $string >> $dest/$file
+    for row in $(echo "${sample}" | jq -r '.[] | @base64'); do
+        _jq() {
+        echo ${row} | base64 --decode | jq -r ${1}
+        }       
+        
+        string="\""
+        t=0
+        flag=$((${#@}-1))
+        for arg in ${@}; do
+            if (( $t != $flag )) 
+            then
+                temp=$(sed 's/\"/\"\"/g' <<< $(_jq ".$arg"))
+                string=$string$temp\", 
+                string=$string\"
+            else
+                temp=$(sed 's/\"/\"\"/g' <<< $(_jq ".$arg"))
+                string=$string$temp\"
+            fi
+            (( t++ ))
+        done
+        echo $string >> $dest/$file
+    done
+done
