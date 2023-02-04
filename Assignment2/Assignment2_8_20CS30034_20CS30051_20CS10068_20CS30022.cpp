8374b9cd10ce64774e92ede06f5efc0368ce7303
Mradul Agrawal <mradul@kgpian.iitkgp.ac.in>
2023-02-05 00:18:31 +0530

    Parsed the string using cpp functions and store the tokens in a vector

--- Assignment2/Assignment2_8_20CS30034_20CS30051_20CS10068_20CS30022.cpp
+++ Assignment2/Assignment2_8_20CS30034_20CS30051_20CS10068_20CS30022.cpp
@@ -3,6 +3,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h> 
+#include <vector> 
 using namespace std; 
 
 int main()
@@ -33,15 +34,9 @@ int main()
         // userInput contain all the characters excluding the newline character
 
         // Parse the userInput
-        char * args[100];
-        for(int i = 0; i < 100; i++)
-        {
-            args[i] = NULL;
-        }
-        int numArgs = 0;
-        int argPointer = 0; 
-
-        bool readingArg = 0; 
+        vector<string> tokens; 
+        bool readingArg = 0;
+        int tokenPtr = -1; 
         
         for(int i = 0; i < inputSize; i++)
         {
@@ -53,27 +48,34 @@ int main()
             {
                 if(readingArg)
                 {
-                    args[numArgs-1][argPointer] = userInput[i];
-                    argPointer++;
+                    tokens[tokenPtr] += userInput[i];
                 }
                 else
                 {
                     readingArg = true; 
-                    numArgs++;
-                    argPointer = 0; 
-                    args[numArgs-1] = (char *) malloc(sizeof(char) * 100);
-                    memset(args[numArgs-1], 0, 100);
-                    args[numArgs-1][argPointer] = userInput[i];
-                    argPointer++;
+                    tokenPtr++;
+                    tokens.push_back("");
+                    tokens[tokenPtr] += userInput[i];
                 }
             }
+        }
+
+        char ** args = new char*[tokens.size() + 1];
+        for(int i = 0; i < tokens.size(); i++)
+        {
+            args[i] = new char[tokens.size()+1];
+            memset(args[i], 0, tokens.size()+1);
+            strcpy(args[i], tokens[i].c_str());
         }
 
         // Spawn a child process to run the input command
         int pid = fork(); 
         if(pid == 0)
         {
-            execvp(args[0], args);
+            if(execvp(args[0], args) < 0)
+            {   
+                cerr << "Cannot find the command: " << args[0] << endl;
+            }
             exit(0); 
         }
         else if(pid > 0)
