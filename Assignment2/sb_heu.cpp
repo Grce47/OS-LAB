0127be6f3b7cf02969fcec5dbe84c2458677556c
Sourabh Soumyakanta Das <96536342+Sourabh-S-Das@users.noreply.github.com>
2023-02-07 17:52:24 +0530

    Create sb_heu.cpp

---
+++ Assignment2/sb_heu.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <dirent.h>
+
+using namespace std;
+
+void p_tree(map<int, vector<int>> &ptree, map<int, int> &par)
+{
+    DIR *dir = opendir("/proc");
+    if(dir == NULL)
+    {
+        perror("Can't be open the /proc file\n");
+        return;
+    }
+
+    struct dirent *entry;
+    while((entry = readdir(dir)) != NULL)
+    {
+        if(entry->d_type != DT_DIR) continue;
+
+        char *endptr;
+        int pid = strtol(entry->d_name, &endptr, 10);
+        if(*endptr != '\0') continue;
+
+        char path[32];
+        sprintf(path,"/proc/%d/status", pid);
+
+        FILE *status_file = fopen(path, "r");
+        if(status_file == NULL) continue;
+
+        char line[256];
+        while(fgets(line, sizeof(line), status_file) != NULL)
+        {
+            if(strncmp(line, "PPid:", 5) == 0)
+            {
+                int ppid;
+                sscanf(line + 5, "%d", &ppid);
+                ptree[ppid].push_back(pid);
+                par[pid] = ppid;
+                break;
+            }
+        }
+
+        fclose(status_file);
+    }
+
+    closedir(dir);
+}
+
+void dfs(map<int, vector<int>> &ptree, int pid, int &cnt)
+{
+    cnt++;
+    if(ptree.find(pid) != ptree.end())
+    {
+        for(auto u: ptree[pid]) dfs(ptree, u, cnt);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    int pid = -1, ppid = -1;
+    pid = atoi(argv[1]);
+    vector<int> plist;
+
+    map<int, vector<int>> ptree;
+    map<int, int> par;
+
+    p_tree(ptree, par);
+
+    while(pid > 1)
+    {
+        plist.push_back(pid);
+        ppid = par[pid];
+        printf("Child process = %d  Parent process = %d\n", pid, ppid);
+        pid = ppid;
+    }
+    plist.push_back(pid);
+    printf("\n");
+
+    int itr = 3;
+    vector<vector<int>> childCnt(itr);
+    for(int i = 0; i < itr; i++)
+    {
+        for(auto u: plist)
+        {
+            int cnt = 0;
+            dfs(ptree, u, cnt);
+            printf("%d :: %d\n", u, cnt);
+            childCnt[i].push_back(cnt);
+        }
+        printf("\n");
+
+        ptree.clear();
+        par.clear();
+
+        if(i == itr -  1) break;
+
+        sleep(20);
+        p_tree(ptree, par);
+    }
+
+    vector<double> heu;
+    heu.push_back((double)(childCnt[itr - 1][0] / childCnt[0][0]));
+    for(int i = 1; i < childCnt[0].size(); i++)
+        heu.push_back((childCnt[itr - 1][i] * childCnt[0][i - 1]) / (double)(childCnt[itr - 1][i - 1] * childCnt[0][i]));
+
+    for(int i = heu.size() - 1; i >= 0; i--)
+        if(heu[i] > 1.1)
+        {
+            printf("Possible malware process is %d\n\n", plist[i]);
+            return 0;
+        }
+    printf("No malwares found\n\n");
+    return 0;
+}
