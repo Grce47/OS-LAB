4dea2eea1aad57b78b3f421cd6b1934901bc4b2b
grce47 <gracesharma47@gmail.com>
2023-03-21 01:47:35 +0530

    Grace pushed Tue Mar 21 01:47:35 AM IST 2023

--- Assignment5/main.cpp
+++ Assignment5/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <unistd.h>
+#include <time.h>
 #include <pthread.h>
+#include <set>
 #include <time.h>
 #include <vector>
 #include <assert.h>
@@ -29,21 +31,33 @@ class Room
 {
 public:
     Guest *guest;
-    int previous_guest;
+    int previous_guest_time;
+    int number_of_guests;
     bool cleaned;
     Room()
     {
-        cleaned = true;
-        previous_guest = 0;
+        number_of_guests = 0;
+        previous_guest_time = 0;
         guest = NULL;
     }
 };
 
 int x, y, n;
 Room *rooms;
-sem_t sem_id;
+sem_t *sem_id, sem_cleaner;
+pthread_mutex_t mutex_id;
+pthread_cond_t cond_id;
 pthread_t *guest_tid, *staff_tid;
 vector<Guest> guests;
+multiset<pair<int, int>> room_priority_queue;
+void display()
+{
+    cout << "Priority | Room" << endl;
+    for (auto &ele : room_priority_queue)
+    {
+        cout << ele.first << "\t\t\t" << ele.second << endl;
+    }
+}
 
 void *guest_thread(void *args)
 {
@@ -62,48 +76,102 @@ void *guest_thread(void *args)
     {
         sleep(get_random(10, 20));
 
-        // a case
+        pthread_mutex_lock(&mutex_id);
 
-        // b case
-        // assert no room empty
-
-        for (int i = 0; i < n; i++)
+        while (room_priority_queue.begin()->first >= guest->priority)
         {
-            if (rooms[i].guest->priority < guest->priority)
-            {
-                // do swapping
-            }
+            pthread_cond_wait(&cond_id, &mutex_id);
         }
+        int room_no = room_priority_queue.begin()->second;
+        cout << endl
+             << "Starting queue" << endl;
+        display();
+        cout << "THIS GUEST IS " << guest->priority << endl;
+        cout << "About to get ROOM = " << room_no << " GUEST with Priority = " << guest->priority << endl;
+
+        // rooms[room_no].number_of_guests++;
+        // if (rooms[room_no].number_of_guests <= 2)
+        //     sem_wait(&sem_cleaner);
+
+        cout << "We are Removing " << room_priority_queue.begin()->first << " from room number = " << room_no << " and ";
+        cout << "ADDing " << guest->priority << " from room number = " << room_no << endl;
+
+        if (room_priority_queue.begin()->first != 0)
+        {
+            // intterupt signal
+            cout << "(because queue.first.priority != 0) SEM_POST = " << room_priority_queue.begin()->first << " FROM " << guest->priority << endl;
+            sem_post(&sem_id[room_no]);
+        }
+        room_priority_queue.erase(room_priority_queue.begin());
+        room_priority_queue.insert(make_pair(guest->priority, room_no));
+
+        cout << "After Erasing and inserting" << endl;
+        display();
+        cout << endl;
+
+        rooms[room_no].guest = guest;
+
+        pthread_cond_signal(&cond_id);
+        pthread_mutex_unlock(&mutex_id);
+
+        int random_number = get_random(10, 30);
+        struct timespec sleep_time;
+        clock_gettime(CLOCK_REALTIME, &sleep_time);
+        sleep_time.tv_sec += random_number;
+
+        int res = sem_timedwait(&sem_id[room_no], &sleep_time);
+
+        pthread_mutex_lock(&mutex_id);
+        if (res == 0)
+        {
+            // interrupt
+            // do nothing
+            cout << "GOT INTERRUPT " << guest->priority << " " << endl;
+        }
+        else
+        {
+            // not interrupt
+            cout << "Peace REM " << guest->priority << " " << room_no << " ADD " << 0 << " " << room_no << endl;
+            room_priority_queue.erase(room_priority_queue.find(make_pair(guest->priority, room_no)));
+            room_priority_queue.insert(make_pair(0, room_no));
+            rooms[room_no].guest = NULL;
+            rooms[room_no].previous_guest_time += random_number;
+        }
+
+        pthread_cond_signal(&cond_id);
+        pthread_mutex_unlock(&mutex_id);
     }
     pthread_exit(0);
 }
 
 void *cleaning_staff_thread(void *args)
 {
-    while (1)
-    {
-        // wakes up when required
+    // while (1)
+    // {
+    //     // wakes up when required
 
-        // select room
-        vector<int> idx;
-        for (int i = 0; i < n; i++)
-            if (!rooms[i].cleaned)
-                idx.push_back(i);
+    //     // select room
+    //     vector<int> idx;
+    //     for (int i = 0; i < n; i++)
+    //         if (!rooms[i].cleaned)
+    //             idx.push_back(i);
 
-        int select_idx = idx[get_random(0, idx.size() - 1)];
-        Room *selected_room = &rooms[select_idx];
+    //     int select_idx = idx[get_random(0, idx.size() - 1)];
+    //     Room *selected_room = &rooms[select_idx];
 
-        // do cleaning
-        sleep(selected_room->previous_guest);
-        selected_room->previous_guest = 0;
-        selected_room->cleaned = true;
-    }
+    //     // do cleaning
+    //     sleep(selected_room->previous_guest);
+    //     selected_room->previous_guest = 0;
+    //     selected_room->cleaned = true;
+    // }
 
     pthread_exit(0);
 }
 
 int main()
 {
+    pthread_mutex_init(&mutex_id, NULL);
+    pthread_cond_init(&cond_id, NULL);
     time(NULL);
     cin >> x >> y >> n;
     if (!(y > n && n > x && x > 1))
@@ -121,13 +189,22 @@ int main()
     rooms = new Room[n]();
     guest_tid = new pthread_t[y];
     staff_tid = new pthread_t[x];
+    sem_id = new sem_t[n];
+
+    sem_init(&sem_cleaner, 0, 2 * n);
+
+    for (int i = 0; i < n; i++)
+        sem_init(&sem_id[i], 0, 0);
+
+    for (int i = 0; i < n; i++)
+        room_priority_queue.insert(make_pair(0, i));
 
     for (int i = 0; i < y; i++)
     {
-        Guest guest(get_random(1, y));
+        Guest guest(i + 1);
         guests.push_back(guest);
     }
-
+    display();
     for (int i = 0; i < y; i++)
         pthread_create(&guest_tid[i], NULL, guest_thread, (void *)(&guest_tid[i]));
     for (int i = 0; i < x; i++)
@@ -136,6 +213,9 @@ int main()
         pthread_join(guest_tid[i], NULL);
     for (int i = 0; i < x; i++)
         pthread_join(staff_tid[i], NULL);
+
+    pthread_mutex_destroy(&mutex_id);
+    pthread_cond_destroy(&cond_id);
 
     exit(EXIT_SUCCESS);
 }
